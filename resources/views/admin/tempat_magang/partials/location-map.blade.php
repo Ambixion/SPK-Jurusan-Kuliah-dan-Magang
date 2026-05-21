f95ebbce6ce5a16e6555efea15e106c2d9bdf82c
dgies <dagiuyy111@gmail.com>
2026-05-21 19:14:53 +0700

    Update: fixing Leaflet Views add addres name

--- resources/views/admin/tempat_magang/partials/location-map.blade.php
+++ resources/views/admin/tempat_magang/partials/location-map.blade.php
@@ -215,10 +215,9 @@
         hasCoords ? 15 : 13
     );
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 20,
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 19,
     }).addTo(map);
 
     let marker = null;
